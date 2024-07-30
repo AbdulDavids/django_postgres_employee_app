c0f72445ba89f2c0426ce8c219e7477a4f97fa4a
AbdulDavids <abdulbaaridavids04@gmail.com>
2024-07-30 12:31:42 +0200

    change settings.py to use employees_host

--- employee_app/settings.py
+++ employee_app/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
 		"NAME": os.environ.get("DB_NAME"),
 		"USER": os.environ.get("DB_USER"),
 		"PASSWORD": os.environ.get("DB_PASSWORD"),
-		"HOST": os.environ.get("DB_HOST"),
+		"HOST": os.environ.get("EMPLOYEES_HOST"),
 		"PORT": "5432",
     }
 }
